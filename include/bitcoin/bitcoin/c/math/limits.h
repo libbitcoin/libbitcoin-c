636b604e60b5b491ee34777ce0300612c2a9bb17
Yarvis Hatsa <PoliceTerror@dyne.org>
2016-10-01 05:35:51 -0400

    added limits

---
+++ include/bitcoin/bitcoin/c/math/limits.h
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) 2011-2016 libbitcoin developers (see AUTHORS)
+ *
+ * This file is part of libbitcoin.
+ *
+ * libbitcoin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License with
+ * additional permissions to the one published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option)
+ * any later version. For more information see LICENSE.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+#ifndef LIBBITCOIN_C_MATH_LIMITS_H
+#define LIBBITCOIN_C_MATH_LIMITS_H
+
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/// Constrain a numeric value within a given range.
+size_t bc_range_constrain(size_t value, size_t minimum, size_t maximum);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
+
