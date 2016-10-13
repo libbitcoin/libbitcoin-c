eece4ed41c9df51c6dd2b9e5562e936f636c9525
Yarvis Hatsa <PoliceTerror@dyne.org>
2016-10-13 09:32:17 +0100

    _select -> __select for select_outputs as per proper tagging scheme for namespaces and classes.

--- include/bitcoin/bitcoin/c/wallet/select_outputs.h
+++ include/bitcoin/bitcoin/c/wallet/select_outputs.h
@@ -28,7 +28,7 @@
 extern "C" {
 #endif
 
-void bc_select_outputs_select(bc_points_info_t* out,
+void bc_select_outputs__select(bc_points_info_t* out,
     const bc_output_info_list_t* unspent, uint64_t minimum_value);
 
 #ifdef __cplusplus
--- src/wallet/select_outputs.cpp
+++ src/wallet/select_outputs.cpp
@@ -26,7 +26,7 @@
 
 extern "C" {
 
-void bc_select_outputs_select(bc_points_info_t* out,
+void bc_select_outputs__select(bc_points_info_t* out,
     const bc_output_info_list_t* unspent, uint64_t minimum_value)
 {
     libbitcoin::wallet::select_outputs::select(
